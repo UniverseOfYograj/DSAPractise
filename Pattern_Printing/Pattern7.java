6556d75d57b947fd1ce70691297d70bb536f59c2
Yograj Tripathi <136296357+UniverseOfYograj@users.noreply.github.com>
2026-06-01 02:20:05 +0530

    Create Pattern7.java

---
+++ Pattern_Printing/Pattern7.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String args[]){
+    int cnt=0;
+    for(int i=0;i<5;i++){
+        
+        for(int j=0;j<5-i;j++){
+            char ch=(char)(j+65);
+         System.out.print(ch);
+        }
+        System.out.println();
+    }
+    
+        
+    }
+}
