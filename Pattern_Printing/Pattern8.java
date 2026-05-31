ba695f0e229191c5e89a781797dfc1d489bacdda
Yograj Tripathi <136296357+UniverseOfYograj@users.noreply.github.com>
2026-06-01 02:23:08 +0530

    Create Pattern8.java

---
+++ Pattern_Printing/Pattern8.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String args[]){
+    int cnt=0;
+    for(int i=0;i<5;i++){
+        
+        for(int j=0;j<=i;j++){
+            char ch=(char)(i+65);
+         System.out.print(ch);
+        }
+        System.out.println();
+    }
+    
+        
+    }
+}
