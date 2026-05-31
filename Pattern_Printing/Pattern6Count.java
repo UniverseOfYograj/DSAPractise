c15130933a64f495ca8149c69c146a7e415596e3
Yograj Tripathi <136296357+UniverseOfYograj@users.noreply.github.com>
2026-06-01 02:16:36 +0530

    Create Pattern6Count.java

---
+++ Pattern_Printing/Pattern6Count.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String args[]){
+    int cnt=0;
+    for(int i=0;i<5;i++){
+        
+        for(int j=0;j<=i;j++){
+            System.out.print(++cnt+" ");
+        }
+        System.out.println();
+    }
+    
+        
+    }
+}
