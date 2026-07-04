9bf855855fdb053496a3df0bf736de582f838169
Yograj Tripathi <136296357+UniverseOfYograj@users.noreply.github.com>
2026-07-05 01:33:34 +0530

    Create 228_Missing_Number.java

---
+++ LeetCode/228_Missing_Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        int tS=(n*(n+1))/2;
+        for(int i:nums){
+            sum+=i;
+        }
+        return tS-sum;
+    }
+}
