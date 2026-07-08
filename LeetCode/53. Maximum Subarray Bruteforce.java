21206340fa6c323d93ca036722a9b960cf850e01
Yograj Tripathi <136296357+UniverseOfYograj@users.noreply.github.com>
2026-07-08 14:03:29 +0530

    Create 53. Maximum Subarray Bruteforce.java

---
+++ LeetCode/53. Maximum Subarray Bruteforce.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum>maxi){
+                    maxi=sum;
+                }
+                if(sum<0){
+                    sum=0;
+                }
+            }
+        }
+        return maxi;
+    }
+}
